0a97b2bba46dbf1bf69e86e8b5743e3a16e458c4
Wangari-j <wangarijudy@gmail.com>
2024-02-08 23:32:01 +0300

    cars

---
+++ classes1.py
@@ -0,0 +1,9 @@
+class Cars:
+    def __init__(self,type,model):
+        self.type = type
+        self.model = model
+
+newCar = Cars("Benz","AMG")
+print(newCar)
+print(newCar.type)
+print(newCar.model)
